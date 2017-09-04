e86e9d6e6e88567c64036c094ef8d118c8bd46b6
Shashank Shekher <shashank@abacusdesk.co.in>
2017-09-04 22:55:29 +0530

    Init

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,49 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { DatePicker } from '@ionic-native/date-picker';
+import { LocalNotifications } from '@ionic-native/local-notifications';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  time: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(private navCtrl: NavController, private datePicker: DatePicker, private localNotifications: LocalNotifications) {
+    this.time = new Date(new Date().getTime() + 5 * 3600);
+    console.log(this.time);
+  }
 
+  setReminder() {
+    this.localNotifications.schedule({
+      id: 1,
+      title: 'Single ILocalNotification',
+      text: 'Multi ILocalNotification 2',
+      icon: 'http://example.com/icon.png'
+    });
+  }
+
+  setSchedule() {
+    this.localNotifications.schedule({
+      text: 'Delayed ILocalNotification',
+      at: this.time,
+      led: 'FF0000',
+      sound: null
+   });
+  }
+
+  getDate() {
+    this.datePicker.show({
+      date: new Date(),
+      mode: 'date',
+      allowOldDates: false,
+      allowFutureDates: true,
+      androidTheme: this.datePicker.ANDROID_THEMES.THEME_HOLO_DARK
+    }).then(
+      date => console.log('Got date: ', date),
+      err => console.log('Error occurred while getting date: ', err)
+    );
   }
 
 }
